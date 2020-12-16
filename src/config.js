135a619426646d534e050039a3a1ffdcfb773006
sahilgupta562 <sahilgupta562@gmail.com>
2020-12-16 02:27:06 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  API_URL: window.location.hostname.includes("localhost") ? `https://vault-dev.xion.ai:8443` : `https://${window.location.hostname}:8443`,
-  ACCOUNT_API_URL: window.location.hostname.includes("localhost") ? `https://vault-dev.xion.ai:8445` : `https://${window.location.hostname}:8445`
+  API_URL: window.location.hostname.includes("localhost") ? `https://dev.test.ai:8443` : `https://${window.location.hostname}:8443`,
+  ACCOUNT_API_URL: window.location.hostname.includes("localhost") ? `https://dev.test.ai:8445` : `https://${window.location.hostname}:8445`
 };
